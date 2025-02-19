7dbebce2c33e09e45d30945b9bde498de492cfe5
rpalominosc <r.palominos.c@gmail.com>
2025-02-19 17:12:43 -0300

    Version 1.1

--- cua2024/funcionarios/forms.py
+++ cua2024/funcionarios/forms.py
@@ -4,16 +4,15 @@ from django.forms import ModelForm
 from funcionarios.models import Funcionario, Grado, Departamento, Estado
 
 class FuncionarioForm(ModelForm):
-
+    nombre_funcionario = forms.CharField(widget=forms.TextInput(attrs={"size": "40"}))
+    codigo_funcionario = forms.CharField(widget=forms.TextInput(attrs={"size": "8"}))
+    
     class Meta:
         model = Funcionario
         #fields = ['codigo_funcionario', 'nombre_funcionario', 'grado_funcionario', 'departamento_funcionario', 'estado_funcionario']
         fields = '__all__'
         exclude = ['cua_funcionario']
 
-        #cua_funcionario = forms.CharField(disabled = True)  # Campo solo lectura
-        #widgets = {
-        #   }
 
 class CuaFuncionario(forms.Form):
     
